221e5bc3746e0ddb411ae7d6627634b194e22bb0
dan4uone <legeng300@mail.ru>
2022-03-18 17:39:02 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
+        ArrayList<String> list2 = new ArrayList<>();
+        while (true){
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Введите имя "+ list.add(scanner.nextLine()));
+
+            }
+            break;
+        }
+        while (true) {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Введите имя " + list2.add(scanner.nextLine()));
+
+            }
+            break;
+        }
+        ArrayList<String> A = new ArrayList<>();
+        A.addAll(list);
+        System.out.println(A);
+        ArrayList<String> B = new ArrayList<>();
+        B.addAll(list2);
+        System.out.println(B);
+        ArrayList<String> C = new ArrayList<>();
+        C.addAll(A);
+        Collections.reverse(B);
+        C.addAll(B);
+        Collections.rotate(C.subList(1,6),-1);
+        Collections.rotate(C.subList(1,7),-1);
+        Collections.rotate(C.subList(1,8),-1);
+        Collections.rotate(C.subList(1,9),-1);
+
+        System.out.println(C);
+        C.sort(Comparator.comparingInt(String::length));
+        C.forEach(System.out::println);
+    }
+
+}
